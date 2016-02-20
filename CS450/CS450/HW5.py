30e6f6216c3b826507d0a77e79d153a925361b34
James Richter <jamesrichter@byui.edu>
2016-02-20 12:02:02 -0700

    nn part 2

--- CS450/CS450/HW5.py
+++ CS450/CS450/HW5.py
@@ -6,9 +6,11 @@ Last Updated: 1/6/2016
 
 The neural network is the "brain."
 """
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import normalize
+from sklearn import tree
 import math
 import argparse
 import numpy as np
@@ -25,22 +27,80 @@ args = parser.parse_args()
 iris = load_iris()
 iris.data = normalize(iris.data, axis=0)
 train_data, test_data, train_target, test_target = train_test_split(iris.data, iris.target, test_size=0.30, random_state=2)
-
-nn = Network(train_data, train_target, 3, [6,7,4,3])
-width = train_data[0].__len__()
-nn.width = width
-nn.generateNetwork()
+nn = Network(train_data, train_target, 3, [3])
 nn.activateNetwork(nn.data[0])
-nn.trainNetwork()
+s = []
+t = []
+tries = 0
+i = 0
+while i < 10000:
+   print i,
+   nn.trainNetwork()
+   t.append(nn.testNetwork(test_data, test_target))
+   if i > 100 and t[-1] <= t[-2]:
+      tries+=1
+   else:
+      tries = 0
+   s.append(i)
+   if tries > 20:
+      i = 10001
+   i += 1
+plt.plot(s, t)
+plt.xlabel('number of iterations')
+plt.ylabel('accuracy')
+plt.title('Iris')
+plt.grid(True)
+plt.savefig("nn_iris.png")
+
+#clf = MLPClassifier(algorithm='l-bfgs', alpha=.1, hidden_layer_sizes=(4), random_state=1)
+#clf.fit(train_data, train_target) 
+#i = 0
+#for x in clf.predict(test_data):
+#   num_right = 0
+#   if x == test_target[i]:
+#      num_right += 1
+#   i +=1
+#print "clf accuracy:", num_right / float(i)
 
 
 array = np.loadtxt('./data/pima-indians-diabetes.data', dtype = "U", delimiter=',')
-house_data = array[:,1::]
-house_target = array[:,0]
+house_data = array[:,::-1]
+house_target = array[:,-1]
 house_data = normalize(house_data, axis=1)
 train_data, test_data, train_target, test_target = train_test_split(house_data, house_target, test_size=0.30, random_state=2)
-nn = Network(train_data, train_target, 3, [4,5,4,2])
-width = train_data[0].__len__()
-nn.width = width
-nn.generateNetwork()
+nn = Network(train_data, train_target, 2, [8])
 nn.activateNetwork(nn.data[0])
+plt.clf()
+s = []
+t = []
+tries = 0
+i = 0
+while i < 10000:
+   nn.trainNetwork()
+   print i, 
+   t.append(nn.testNetwork(test_data, test_target))
+   if i > 100 and t[-1] <= t[-2]:
+      tries+=1
+   else:
+      tries = 0
+   s.append(i)
+   if tries > 100:
+      i = 10001
+   i +=1 
+plt.plot(s, t)
+plt.xlabel('number of iterations')
+plt.ylabel('accuracy')
+plt.title('PIMA')
+plt.grid(True)
+plt.savefig("nn_pima.png")
+plt.show()
+
+#clf = MLPClassifier(algorithm='l-bfgs', alpha=.1, hidden_layer_sizes=(8), random_state=1)
+#clf.fit(train_data, train_target) 
+#i = 0
+#for x in clf.predict(test_data):
+#   num_right = 0
+#   if x == test_target[i]:
+#      num_right += 1
+#   i +=1
+#print "clf accuracy:", num_right / float(i)
--- CS450/CS450/include/HW5a.py
+++ CS450/CS450/include/HW5a.py
@@ -2,7 +2,8 @@ import random
 import numpy as np
 import math
 
-LEARNING_RATE = 0.01
+LEARNING_RATE = .9
+random.seed(5)
 
 def sigmoid(x):
    """A sigmoid function with a gradual slope."""
@@ -10,12 +11,12 @@ def sigmoid(x):
       return 1
    if x < -100:
       return 0
-   return 1.0 / (1.0 + math.exp(-4.9*x))
+   return 1.0 / (1.0 + math.exp(-x))
 
 class Neuron:
    """A single neuron in our neural network."""
    def __init__(self, width):
-      self.incoming_weight = [random.random()-.5 for x in range(width)]
+      self.incoming_weight = [(random.random()-.5)/100 for x in range(width)]
       self.value = 0
    def __repr__(self):
       neuronString = str(self.incoming_weight) + '\\' + str(self.value)
@@ -25,12 +26,13 @@ class Network:
    """
    A neural network, used for calculating outputs to the system.
    """
+   # change 3 to [0 0 0 1]
    def categorizeOutput(self):
       i = 0
       self.targets = list(self.targets)
       new_target = [0 for x in range(self.num_outputs)]
       for target in self.targets:
-         new_target[target] = 1
+         new_target[int(target)] = 1
          self.targets[i] = new_target
          new_target = [0 for x in range(self.num_outputs)]
          i+=1
@@ -38,34 +40,52 @@ class Network:
    #calculate the error of the output nodes
    def outputError(self, index):
       self.hidden_layers = np.array(self.hidden_layers)
-      outputs = []
+      i = 0
       for node in self.output_layer:
-         outputs.append(node.value)
-      print outputs
-      target = self.targets[index]
-      target = np.array(target)
-      outputs = np.array(outputs)
-      print target
-      error = (outputs - target) * outputs * (1 - outputs)
-      self.output_error = error
+         target = self.targets[index][i]
+         aj = node.value
+         delta = (aj - target) * aj * (1 - aj)
+         node.output_delta = delta
+         node.new_weights = []
+         j = 0
+         for weight in node.incoming_weight:
+            new_weight = weight - LEARNING_RATE * delta * self.hidden_layers[-1][j].value
+            node.new_weights.append(new_weight)
+            j+=1
+         i += 1
 
    def hiddenError(self, index):
-      output_error = self.output_error
-      for layer in reversed(hidden_layers):
+      self.hidden_layers = np.array(self.hidden_layers)
+      i = 0
+      for layer in reversed(self.hidden_layers):
+         j = 0
          for node in layer:
-            error = 5
-         output_error = error
-      
-      
-      weight_matrix = []
-      for node in self.hidden_layers[:,-1]:
-         weight_matrix.append(node.incoming_weight)
-      error_matrix = np.array(weight_matrix) * np.array(self.output_error).transpose() 
-      error_matrix = error_matrix
-      self.hidden_error = error
+            addend = 0
+            aj = node.value
+            if i == 0:
+               for node2 in self.output_layer:
+                  addend += node2.incoming_weight[j] * node2.output_delta
+            else:
+               for node2 in (self.hidden_layers[::-1])[i-1][:-1]:
+                  addend += node2.incoming_weight[j] * node2.delta
+            j+=1
+            node.delta = aj*(1-aj)*addend
+            node.new_weights = []
+            k = 0
+            for weight in node.incoming_weight:
+               try:
+                  node.new_weights.append(weight - node.delta*LEARNING_RATE*(self.hidden_layers[::-1])[i+1][k].value)
+               except:
+                  node.new_weights.append(weight - node.delta*LEARNING_RATE*self.input_layer[k].value)
+               k +=1
+         i+=1
 
    def updateWeights(self):
-      pass
+      for layer in self.hidden_layers:
+         for node in layer:
+            node.incoming_weight = node.new_weights
+      for node in self.output_layer:
+         node.incoming_weight = node.new_weights
 
    def __init__(self, data, target, num_outputs, layers):
       self.layers = layers
@@ -78,26 +98,70 @@ class Network:
       self.output_layer = []
       self.generateNetwork()
 
+   def __repr__(self):
+      ret_string = ""
+      ret_string+="Layers: "
+      ret_string+=" " + str(self.input_layer.__len__())
+      for x in self.hidden_layers:
+         ret_string+=" " + str(x.__len__())
+      ret_string+=" " + str(self.output_layer.__len__())
+      ret_string+="\nWeights: "
+      for x in self.hidden_layers:
+         for node in x:
+            for weight in node.incoming_weight:
+               ret_string+=str(weight) + " "
+            ret_string+='\n'
+         ret_string+='\n'
+      for node in self.output_layer:
+         for weight in node.incoming_weight:
+            ret_string+=str(weight) + " "
+         ret_string+='\n'
+      return ret_string
+
    def trainNetwork(self):
-      self.categorizeOutput()
       i = 0
       for data_pt in self.data:
          self.activateNetwork(data_pt)
          self.outputError(i)
          self.hiddenError(i)
+         self.updateWeights()
          i+=1
 
+   def testNetwork(self, test_data, test_target):
+      i = 0
+      num_right = 0
+      for datapt in test_data:
+         self.activateNetwork(datapt)
+         values = []
+         for node in self.output_layer:
+            values.append(node.value)
+         if int(values.index(max(values))) == int(test_target[i]):
+            num_right +=1
+         i+=1
+
+      return num_right/float(i)
+
    def generateNetwork(self):
-      self.input_layer = [Neuron(0) for count in range(self.width + 1)]
+      self.categorizeOutput()
+      self.input_layer = [Neuron(0) for count in range(self.data[0].__len__() + 1)]
       self.hidden_layers = []
       i = 0
       for x in self.layers:
          if i == 0:
-            self.hidden_layers.append([Neuron(self.width + 1) for count in range(x + 1)])
+            self.hidden_layers.append([Neuron(self.data[0].__len__() + 1) for count in range(x + 1)])
          else:
             self.hidden_layers.append([Neuron(self.layers[i-1] + 1) for count in range(x + 1)])
          i += 1
       self.output_layer = [Neuron(self.layers[-1]+1) for count in range(self.num_outputs)]
+
+   def flushNetwork(self):
+      for layer in self.hidden_layers:
+         for node in layer:
+            node.value = 0
+         layer[-1].value = 1
+            
+      for node in self.output_layer:
+         node.value = 0
 
    def activateNetwork(self, inputs):
       """
@@ -105,6 +169,7 @@ class Network:
       """
       #outputs = {}
       assert self.input_layer.__len__() == inputs.__len__() + 1
+      self.flushNetwork()
 
       # plug in the inputs
       for x in range(inputs.__len__()):
@@ -138,4 +203,4 @@ class Network:
             i+=1
          neuron.value = sigmoid(neuron.value)
 
-      print "done :)"
+      #print "done :)"
